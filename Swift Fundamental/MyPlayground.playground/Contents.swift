cbbbd9ec135f12be6d1afca68699e80d05634430
Alvi Hasan <livealvi@gmail.com>
2021-12-15 17:36:25 +0600

    Variables and Constants

--- Swift Fundamental/MyPlayground.playground/Contents.swift
+++ Swift Fundamental/MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var health : Int = 100
+health = 50
+
+var currentHealth : Int = health
+
+let maxHealth : Int = 100
+
+health = maxHealth
+
+health = 80
